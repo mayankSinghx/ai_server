0b4367e390844e487a63ab2b8200a8e3e22bcb5c
mayankxcrino <mayank@xcrino.com>
2026-03-29 01:21:00 +0530

    added domains

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,13 @@ import blogAgent from "../utils/blog-agent"
 
 const app = express()
 
-app.use(cors())
+app.use(cors(
+    {
+        origin: ["ai-todo-puce.vercel.app", "http://localhost:3000"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
+    }
+))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use("/uploads", express.static("uploads"))
